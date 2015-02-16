e368bbb4cb076394620008738fa86444f75fc3f4
vittoriourzi <vittoriourzi@192.168.1.64>
2015-02-16 21:43:34 +0100

    first commit

---
+++ application/views/frontend/static/footer.php
@@ -0,0 +1,78 @@
+<!-- Footer -->  
+<div class="footer-big">
+    <div class="container">
+        <div class="row">
+            <!-- About Us Information -->
+            <div class="col-md-3 col-sm-6">                           
+                <div class="f-logo">
+                    <h2>Our<br>Offices</h2>                                                                    
+                </div>
+            </div>
+            <!-- Recent Posts -->
+            <div class="col-md-3 col-sm-6 f-inner">                        
+                <div class="col-sm-2">
+                    <i class="fa fa-map-marker fa-lg"></i> 
+                </div>
+                <div class="col-sm-10">
+                    <strong>New York</strong>
+                    <div class="f-space"></div>
+                    <address>                                
+                        795 Folsom Ave, Suite 600<br>                                
+                        <abbr title="Phone">P:</abbr> (123) 456-7890
+                        first.last@example.com
+                    </address>                           
+                </div>
+            </div>
+            <!-- Latest Tweet -->
+            <div class="col-md-3 col-sm-6 f-inner">
+                <div class="col-sm-2">
+                    <i class="fa fa-map-marker fa-lg"></i> 
+                </div>
+                <div class="col-sm-10">
+                    <strong>London</strong>
+                    <div class="f-space"></div>
+                    <address>                                
+                        795 Folsom Ave, Suite 600<br>                                
+                        <abbr title="Phone">P:</abbr> (123) 456-7890
+                        first.last@example.com
+                    </address>
+                </div>
+            </div>
+            <!-- Recent Works -->
+            <div class="col-md-3 col-sm-6 f-inner">   
+                <div class="col-sm-2">
+                    <i class="fa fa-map-marker fa-lg"></i> 
+                </div>
+                <div class="col-sm-10">
+                    <strong>Berlin</strong>
+                    <div class="f-space"></div>
+                    <address>                                
+                        795 Folsom Ave, Suite 600<br>                                
+                        <abbr title="Phone">P:</abbr> (123) 456-7890
+                        first.last@example.com
+                    </address>
+                </div>      
+            </div>                           
+        </div>
+    </div>
+</div>   
+<div id="footer">
+    <div class="container">   
+        <div class="row">                                                         
+            <!-- Copyright -->                                                       
+            <div class="col-sm-6 col-md-6 f-copyright">                        
+                <span>© Copyright LARX - All Rights Reserved</span>                        
+            </div>                      
+            <div class="col-sm-6 col-md-6">
+                <!-- Social Icons -->                              
+                <ul class="footer-social">
+                    <li><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-skype fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li><a href="#"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-pinterest fa-stack-1x fa-inverse"></i></span></a></li>
+                </ul>
+            </div>
+        </div><!-- /row -->
+    </div><!-- /container -->
+</div>        
+<!-- End Footer -->
---
+++ application/views/frontend/static/header.php
@@ -0,0 +1,43 @@
+<!-- Navbar -->
+<nav class="navbar navbar-default navbar-fixed-top header-nav" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- Logo -->        
+            <a class="navbar-brand" href="index-m.html"><img height="24px" src="<?=base_url()?>assets/img/brand.png"></a>                     
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="index-m.html">Home</a></li>
+                <li><a href="about.html">About</a></li>                                                                            
+                <li><a href="projects.html">Projects</a></li>                    
+                <li><a href="services.html">Services</a></li>
+                <li><a href="blog.html">Blog</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Pages <i class="fa fa-angle-down fa-lg"></i></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="onepage_dark.html">One Page Dark</a></li>
+                        <li><a href="onepage_slider.html">One Page Slider</a></li>
+                        <li><a href="onepage_video.html">One Page Video</a></li>
+                        <li><a href="onepage_nav_bottom.html">One Page Bottom Nav</a></li>
+                        <li><a href="onepage-canvas.html">One Page Off-Canvas</a></li>
+                        <li><a href="index-m.html">Multi-Page</a></li>
+                        <li class="divider"></li>
+                        <li><a href="shortcodes.html">Shortcodes</a></li>    
+                        <li><a href="404_page.html">404 Error</a></li>                                    
+                        <li><a href="coming_soon.html">Coming Soon</a></li>                                    
+                    </ul>
+                </li>                        
+                <li><a href="contact.html">Contact</a></li>  
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+<!-- Navbar -->
